b678414253fe3907731470e8728cd351dcb72506
Sahil Kumar Singh <152484304+Sahil2664@users.noreply.github.com>
2026-07-10 23:53:08 +0530

    feat: complete React Exercise 1 (myfirstreact)

---
+++ Module-9-React/myfirstreact/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="welcome-heading">welcome to the first session of React</h1>
+    </div>
+  )
+}
+
+export default App
