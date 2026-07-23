e33231a707765c61429b19c93a639a958f76e5dd
Sahil Kumar Singh <152484304+Sahil2664@users.noreply.github.com>
2026-07-24 00:14:31 +0530

    feat: complete all Angular hands-on modules (1-10)

--- Angular_HandsOn/sahil/student-course-portal/src/app/directives/highlight.spec.ts
+++ Angular_HandsOn/sahil/student-course-portal/src/app/directives/highlight.spec.ts
@@ -1,8 +1,10 @@
 import { Highlight } from './highlight';
+import { ElementRef } from '@angular/core';
 
 describe('Highlight', () => {
   it('should create an instance', () => {
-    const directive = new Highlight();
+    const mockElementRef = new ElementRef(document.createElement('div'));
+    const directive = new Highlight(mockElementRef);
     expect(directive).toBeTruthy();
   });
 });
